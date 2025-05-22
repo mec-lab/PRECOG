2e9c60ce916c485d4ad114e942de35dd5a58f2d8
ambertsc <ambertsc@uvm.edu>
2025-05-22 07:22:01 -0400

    Update search.py

--- search.py
+++ search.py
@@ -3,7 +3,7 @@ from parallelHillClimber import PARALLEL_HILL_CLIMBER
 import sys 
 
 run_num = sys.argv[1]
-run_name = 'bm_quad_10k'+str(run_num)
+run_name = 'quadruped'+str(run_num)
 phc = PARALLEL_HILL_CLIMBER(run_name, run_num)
 phc.Evolve("DIRECT")
 # phc.Show_Best()
